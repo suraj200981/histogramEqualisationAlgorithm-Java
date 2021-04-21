ad4e7e753e4334387d92f918d37f980dfdf7c565
ss2261 <ss2261@kent.ac.uk>
2021-04-21 14:13:33 +0100

    cumulative sum calculated

--- src/MainAlg.java
+++ src/MainAlg.java
@@ -1,7 +1,7 @@
 public class MainAlg {
     public static void main(String[] args) {
         int [] greyLevelHistogram = new int[8];
-        greyLevelHistogram[0]= 780;
+        greyLevelHistogram[0]= 790;
         greyLevelHistogram[1]= 1023;
         greyLevelHistogram[2]= 850;
         greyLevelHistogram[3]= 656;
@@ -19,12 +19,12 @@ public class MainAlg {
 
     public static void alg(int [] inputArr, int bit){
 
-
+        int [] sumArr = new int[inputArr.length];
         //g
         System.out.print("g:      ");
     for(int x=0; x<inputArr.length; x++){
 
-        System.out.print(x +"     ");
+        System.out.print(x +"        ");
 
     }
         System.out.println();
@@ -34,7 +34,23 @@ public class MainAlg {
 
         for(int x=0; x<inputArr.length; x++){
 
-            System.out.print(inputArr[x] +"   ");
+            System.out.print(inputArr[x] +"      ");
+
+        }
+
+        System.out.println();
+        //Cumulative frequency. The sum of h(g)
+        System.out.print("Σh(g): ");
+        int total = 0;
+        for(int x=0; x<inputArr.length; x++){
+            total= total+ inputArr[x];
+            sumArr[x]=total;
+            /*if(x>0) {
+                for(int i=1; i<inputArr.length; i++) {
+                    sumArr[i] = inputArr[i-1] + inputArr[i + 1];
+                }
+            }*/
+            System.out.print(sumArr[x] +"     ");
 
         }
 
